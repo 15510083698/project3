1f803252511c2ee1390cbf61f71ea7c1f3382678
18518881233 <3216392022@qq.com>
2019-12-17 18:29:43 +0800

    new file jal

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <Shop></Shop>
     <router-view/>
   </div>
 </template>
 
 <script>
+import './assets/js/remStyle.js'
+import Shop from './components/shop'
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    Shop
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/css/reset.css'
 </style>
